bae200a8bfabf9c48db86d43a3d595211fd50253
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2022-12-06 19:56:33 +0530

    create basic structure of emoji kit

---
+++ src/components/Emoji.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Emoji.css'
+
+function Emoji() {
+  return (
+   <>
+   <div className='background-emoji'>
+   <div className='container'>
+    <div className='app-title-container shadow-lg'>
+        <h1 className='app-title text-center'>EMOJI-KIT🤡</h1>
+    </div>
+
+
+    <div className='emoji-container'>
+          <div className='emoji text-center'>
+          ❤️
+          </div>
+    </div>
+   </div>
+   </div>
+   </>
+  )
+}
+
+export default Emoji
