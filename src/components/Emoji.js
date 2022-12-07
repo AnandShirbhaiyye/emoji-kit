c29ff4f805d6c3b64b042caf6c9af5c52bc19e60
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2022-12-07 18:17:36 +0530

    create mini project emoji kit project using react

--- src/components/Emoji.js
+++ src/components/Emoji.js
@@ -1,88 +1,92 @@
-import React from 'react';
-import { useState } from 'react';
-import './Emoji.css'
+import React from "react";
+import { useState } from "react";
+import "./Emoji.css";
 
 function Emoji() {
   const [emoji, setEmoji] = useState("😍");
   return (
-   <>
-   <div className='background-emoji'>
-   <div className='container'>
-    <div className='app-title-container shadow-lg'>
-        <h1 className='app-title text-center'>EMOJI-KIT🤡</h1>
-    </div>
-
-
-    <div className='emoji-container'>
-          <div className='emoji text-center'>
-          {emoji}
+    <>
+      <div className="background-emoji">
+        <div className="container">
+          <div className="app-title-container shadow-lg">
+            <h1 className="app-title text-center">EMOJI-KIT🤡</h1>
           </div>
-    </div>
 
-    <div className='emoji-container-emoji'>
-      <div className='emoji'
-      onClick={()=>{
-        setEmoji("😎")
-      }}>
-        😎
+          <div className="emoji-container">
+            <div className="emoji text-center">{emoji}</div>
+          </div>
+
+          <div className="emoji-container-emoji">
+            <div
+              className="emoji"
+              onClick={() => {
+                setEmoji("😎");
+              }}
+            >
+              😎
+            </div>
+
+            <div
+              className="emoji"
+              onClick={() => {
+                setEmoji("😇");
+              }}
+            >
+              😇
+            </div>
+
+            <div
+              className="emoji"
+              onClick={() => {
+                setEmoji("🤩");
+              }}
+            >
+              🤩
+            </div>
+
+            <div
+              className="emoji"
+              onClick={() => {
+                setEmoji("😶");
+              }}
+            >
+              😶
+            </div>
+
+            <div
+              className="emoji"
+              onClick={() => {
+                setEmoji("🥰");
+              }}
+            >
+              🥰
+            </div>
+
+            <div
+              className="emoji"
+              onClick={() => {
+                setEmoji("🎯");
+              }}
+            >
+              🎯
+            </div>
+
+            <div
+              className="emoji"
+              onClick={() => {
+                setEmoji("🎁");
+              }}
+            >
+              🎁
+            </div>
+          </div>
+        </div>
+        <div className="footer-container text-center mt-4">
+          <h3>ThankYou...🙏</h3>
+        </div>
       </div>
-
-      <div className='emoji'
-      onClick={()=>{
-        setEmoji("😇")
-      }}>
-        😇
-      </div>
-
-      <div className='emoji'
-      onClick={()=>{
-        setEmoji("🤩")
-      }}>
-        🤩
-      </div>
-
-      <div className='emoji'
-      onClick={()=>{
-        setEmoji("😶")
-      }}>
-        😶
-      </div>
-
-      <div className='emoji'
-      onClick={()=>{
-        setEmoji("🥰")
-      }}>
-        🥰
-      </div>
-
-      <div className='emoji'
-      onClick={()=>{
-        setEmoji("🎯")
-      }}>
-        🎯
-      </div>
-
-      <div className='emoji'
-      onClick={()=>{
-        setEmoji("🎁")
-      }}>
-        🎁
-      </div>
-
-      
-  
-
-    </div>
-   
-   </div>
-   <div className='footer-container text-center mt-4'>
-    <h3>ThankYou...🙏</h3>
-   </div>
-   </div>
-
- 
-   </>
-  )
+    </>
+  );
 }
 
-export default Emoji
+export default Emoji;
